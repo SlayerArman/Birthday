55c5a1bf9ef4d0c5419bc3fbc100bd23a6220849
SlayerArman <cartoontv123360@gmail.com>
2026-08-21 15:16:51 +0500

    Added Interaction to Letter

--- js/script.js
+++ js/script.js
@@ -1,8 +1,34 @@
 const lightbox = document.querySelector("#lightbox");
 const lightboxImage = document.querySelector(".lightbox__image");
 const lightboxTriggers = document.querySelectorAll("[data-lightbox]");
-const lightboxCloseButtons =
-    document.querySelectorAll("[data-lightbox-close]");
+const lightboxCloseButtons = document.querySelectorAll("[data-lightbox-close]");
+const letter = document.querySelector("#letter");
+const openLetterButton = document.querySelector("#openLetter");
+
+function openLetter() {
+    letter.classList.add("is-open");
+    letter.setAttribute("aria-hidden", "false");
+    document.body.style.overflow = "hidden";
+}
+
+function closeLetter() {
+    letter.classList.remove("is-open");
+    letter.setAttribute("aria-hidden", "true");
+
+    document.body.style.overflow = "";
+}
+
+openLetterButton.addEventListener("click", openLetter);
+
+openLetterButton.forEach((button) => {
+    button.addEventListener("click", closeLetter);
+});
+
+document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape"){
+        closeLetter();
+    }
+});
 
 function openLightbox(src, alt){
     lightboxImage.src = src;
